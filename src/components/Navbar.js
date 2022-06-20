7b15e8a17af8e36797442ccdbf33cf3d26fe16e2
SabineZilde <sabine.zilde@protonmail.ch>
2022-06-21 00:15:15 +0300

    feat: toggle reorder icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Navbar.css';
-import ReorderIcon from '@mui/icons-material/Reorder';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from "@mui/icons-material/Reorder";
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
   return (
-    <div className='navbar'>
-        <div className="toggleButton">
-            <button>
-                <ReorderIcon />
-            </button>
-        </div>
-        <div className="links">
-            <Link to='/'> Home </Link>
-            <Link to='/projects'> Projects </Link>
-            <Link to='/experience'> Experience </Link>
-        </div>
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="links">
+        <Link to="/"> Home </Link>
+        <Link to="/projects"> Projects </Link>
+        <Link to="/experience"> Experience </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
