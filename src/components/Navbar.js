2c947127ac88268b9d0ae5de10d392d56deee30c
SabineZilde <sabine.zilde@protonmail.ch>
2022-06-20 19:26:51 +0300

    feat: adding reorder icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
   return (
     <div className='navbar'>
         <div className="toggleButton">
-            <button></button>
+            <button>
+                <ReorderIcon />
+            </button>
         </div>
         <div className="links">
             <Link to='/'> Home </Link>
