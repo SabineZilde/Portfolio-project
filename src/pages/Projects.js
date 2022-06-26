57f56722c6bb692a376dd6143f4a0fdd90282384
SabineZilde <sabine.zilde@protonmail.ch>
2022-06-26 15:09:29 +0300

    feat: adding projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import Proj1 from "../assets/mms.png";
+import Proj2 from "../assets/serveimage.jpg";
+import '../styles/Projects.css'
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="projects">
+      <h1>My personal projects</h1>
+      <div className="projectList">
+        <ProjectItem name="Social Media Website" image={Proj1} />
+        <ProjectItem name="Project 2" image={Proj2} />
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
