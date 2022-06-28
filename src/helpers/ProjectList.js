851f30bc394f8695effdffdc9cd034394c4f7ab7
SabineZilde <sabine.zilde@protonmail.ch>
2022-06-28 16:09:41 +0300

    feat: project display page

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -3,19 +3,27 @@ import Proj2 from '../assets/serveimage.jpg';
 
 export const ProjectList = [
     {
+        id: 0,
         title: 'Proje name 1',
-        image: Proj1
+        image: Proj1,
+        skills: 'HTML, CSS, JavaScript, React'
     },
     {
-        ntitleame: 'Proj name 2',
-        image: Proj2
+        id: 1,
+        title: 'Proj name 2',
+        image: Proj2,
+        skills: 'HTML, CSS, JavaScript, React'
     },
     {
+        id: 2,
         title: 'Proje name 1',
-        image: Proj1
+        image: Proj1,
+        skills: 'HTML, CSS, JavaScript, React'
     },
     {
-        ntitleame: 'Proj name 2',
-        image: Proj2
+        id: 3,
+        title: 'Proj name 2',
+        image: Proj2,
+        skills: 'HTML, CSS, JavaScript, React'
     },
 ];
---
+++ src/pages/ProjectDisplay.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { ProjectList } from "../helpers/ProjectList";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+function ProjectDisplay() {
+  const { id } = useParams();
+  const project = ProjectList[id];
+  return (
+    <div className="project">
+      <h1>{project.title}</h1>
+      <img src={project.image} alt={project.title} />
+      <p><b>Skills:</b> {project.skills}</p>
+      <GitHubIcon />
+    </div>
+  );
+}
+
+export default ProjectDisplay;
