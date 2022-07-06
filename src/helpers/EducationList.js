85976340b0f2ad224a2d407e259931b807c29721
SabineZilde <sabine.zilde@protonmail.ch>
2022-07-06 23:41:24 +0300

    feat: formal educattion list

---
+++ src/helpers/EducationList.js
@@ -0,0 +1,16 @@
+export const EducationList = [
+    {
+        name: 'Riga Technical University',
+        degree: 'MScEng, Digital Humanities',
+        description: 'Master Thesis: “Developing Guidelines for Readers and Creators to Improve Data Visualization Literacy”',
+        date: '2018 - 2020',
+        type: 'formal'
+    },
+    {
+        name: 'University of Latvia',
+        degree: 'BFA, Graphic Design',
+        description: 'Diploma thesis: “Infographic as a Modern Visual Communication Type”',
+        date: '2008 - 2014',
+        type: 'formal'
+    }
+]
