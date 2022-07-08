07948019ed6a3fdad84fa9540f1134a104b73782
SabineZilde <sabine.zilde@protonmail.ch>
2022-07-08 22:45:27 +0300

    info: online courses

--- src/helpers/EducationList.js
+++ src/helpers/EducationList.js
@@ -1,16 +1,30 @@
 export const EducationList = [
     {
+        name: 'Riga TechGirls & Accenture Baltics',
+        degree: 'She Goes Tech',
+        description: 'JavaScript Bootcamp (220 h)',
+        date: '02.2021 - 07.2021',
+        type: 'online'
+    },
+    {
+        name: 'Riga TechGirls',
+        degree: 'Mentorship Progarm',
+        description: 'The Riga TechGirls mentorship program aimed to provide support and help to develop skills for women who work in the field of technology or have just started it.',
+        date: '01.2021 - 06.2021',
+        type: 'online'
+    },
+    {
         name: 'Riga Technical University',
         degree: 'MScEng, Digital Humanities',
         description: 'Master Thesis: “Developing Guidelines for Readers and Creators to Improve Data Visualization Literacy”',
-        date: '2018 - 2020',
+        date: '09.2018 - 07.2020',
         type: 'formal'
     },
     {
         name: 'University of Latvia',
         degree: 'BFA, Graphic Design',
         description: 'Diploma thesis: “Infographic as a Modern Visual Communication Type”',
-        date: '2008 - 2014',
+        date: '09.2008 - 02.2014',
         type: 'formal'
     }
 ]
