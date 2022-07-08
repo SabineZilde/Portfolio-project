ccef1680120ff47752d8f0ff99994198e9cca04e
SabineZilde <sabine.zilde@protonmail.ch>
2022-07-08 23:27:20 +0300

    fix: data format

--- src/helpers/EducationList.js
+++ src/helpers/EducationList.js
@@ -3,28 +3,28 @@ export const EducationList = [
         name: 'Riga TechGirls & Accenture Baltics',
         degree: 'She Goes Tech',
         description: 'JavaScript Bootcamp (220 h)',
-        date: '02.2021 - 07.2021',
+        date: 'Feb 2021 - Jul 2021',
         type: 'online'
     },
     {
         name: 'Riga TechGirls',
         degree: 'Mentorship Progarm',
         description: 'The Riga TechGirls mentorship program aimed to provide support and help to develop skills for women who work in the field of technology or have just started it.',
-        date: '01.2021 - 06.2021',
+        date: 'Jan 2021 - Jun 2021',
         type: 'online'
     },
     {
         name: 'Riga Technical University',
         degree: 'MScEng, Digital Humanities',
         description: 'Master Thesis: “Developing Guidelines for Readers and Creators to Improve Data Visualization Literacy”',
-        date: '09.2018 - 07.2020',
+        date: 'Sep 2018 - Jul 2020',
         type: 'formal'
     },
     {
         name: 'University of Latvia',
         degree: 'BFA, Graphic Design',
         description: 'Diploma thesis: “Infographic as a Modern Visual Communication Type”',
-        date: '09.2008 - 02.2014',
+        date: 'Sep 2008 - Feb 2014',
         type: 'formal'
     }
 ]
--- src/helpers/ExperienceList.js
+++ src/helpers/ExperienceList.js
@@ -3,54 +3,54 @@ export const ExperienceList = [
         title: 'Ad Operations Trafficker',
         type: 'Full-time',
         company: 'Setupad',
-        date: '2022 - ongoing'
+        date: 'Jul 2022 - ongoing'
     },
     {
         title: 'Graphic Designer',
         type: 'Freelance',
         company: 'Various non-profit/pro bono individual projects',
-        date: '2014 - ongoing'
+        date: 'Mar 2014 - ongoing'
     },
     {
         title: 'Junior Frontend Developer',
         type: 'Internship',
         company: 'Accenture Baltics',
-        date: '09 - 12.2021'
+        date: 'Sep 2021 - Dec 2021'
     },
     {
         title: 'Front Office & Revenue Manager',
         type: 'Full-time',
         company: 'Airport Hotel Māra',
-        date: '2018 - 2020'
+        date: 'Jan 2018 - Sep 2020'
     },
     {
-        title: 'Front Office Manager <- FO supervisor <- FO Receptionist',
+        title: 'FO Manager <- FO supervisor <- FO Receptionist',
         type: 'Full-time',
         company: 'Best Western Hotel Māra',
-        date: '2010 - 2018'
+        date: 'Aug 2010 - Dec 2017'
     },
     {
         title: 'Graphic Designer',
         type: 'Internship',
         company: 'Rija Films',
-        date: '2012 - 2013'
+        date: 'Dec 2012 - Apr 2013'
     },
     {
         title: 'Graphic Designer',
         type: 'Internship',
         company: 'Jurmala Primarry school "Atvase"',
-        date: '08 -11.2011'
+        date: 'Aug 2011 - Nov 2011'
     },
     {
         title: 'Graphic Designer',
         type: 'Internship',
         company: 'DUE design',
-        date: '07 - 08.2011'
+        date: 'Jul 2011 - Aug 2011'
     },
     {
         title: 'Rental Sales Agent',
         type: 'Full-time',
         company: 'Budget Car Rental',
-        date: '2007 - 2009'
+        date: 'Aug 2007 - Apr 2009'
     }
 ];
