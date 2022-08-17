41c221ce7f82b6f53c3e9f9062d648a3bb83a9f2
SabineZilde <sabine.zilde@protonmail.ch>
2022-08-17 19:32:04 +0300

    feat: adding loader

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ProjectItem from "../components/ProjectItem";
 import '../styles/Projects.css';
 import { ProjectList } from "../helpers/ProjectList";
+import Loader from "../components/Loader";
 
 function Projects() {
   return (
     <div className="projects">
       <h1>My personal projects</h1>
+      <Loader />
       <div className="projectList">
         {ProjectList.map((project, idx) => {
           return <ProjectItem title={project.title} image={project.image} id={idx} key={idx} />;
